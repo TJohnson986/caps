1676e58239bef86fbef1852cf34a7d30ef55e9ac
Tyler Johnson <tyler@csdconsultants.us>
2021-06-30 16:53:57 -0700

    refactoring code to use socket io rather than node events. created server dir to take place of caps.js, refactored vendor.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 'use strict';
 
 const io = require('socket.io');
+const faker = require('faker');
 
 const server = io(3000);
 
 server.on('connection', (socket) => {
-  console.log('client connected', socket.id);
-  socket.emit('success', 'yes');
+  console.log('client connected: ' +socket.id);
+
+  // socket.on('newOrder', ()=> {
+  //   server.emit('pickup', order);
+  // });
+
+  socket.on('pickup', (order) => {
+    server.emit('inTransit', order);
+  })
+  
+  socket.on('inTransit')
 
 });
--- vendor/order-handler.js
+++ vendor/order-handler.js
@@ -1,9 +1,25 @@
 'use strict';
 
-const events = require('../events.js');
+const io = require('socket.io-client');
+const faker = require('faker');
 
-module.exports = (order) => {
+const socket = io.connect('http://localhost:3000');
+
+setInterval(() => {
+  const order = {
+    randomStore: faker.company.companyName(),
+    randomOrderId: faker.finance.routingNumber(),
+    randomName: faker.name.findName(),
+    randomAddress: faker.address.streetAddress()
+  }
   console.log('-----NEW ORDER-----------------------------------------------------')
   console.log(order);
-  events.emit('pickup', order);
-}
+
+}, 500);
+
+
+// module.exports = (order) => {
+//   console.log('-----NEW ORDER-----------------------------------------------------')
+//   console.log(order);
+//   events.emit('pickup', order);
+// }
